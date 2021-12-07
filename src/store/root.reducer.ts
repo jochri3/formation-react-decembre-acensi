a03af24499759b1f17f6a88f9ae5561c823ea8bb
Christian Lisangola <jochri3@gmail.com>
2021-12-07 13:55:17 +0300

    add api loading and error in root reducer

--- src/store/root.reducer.ts
+++ src/store/root.reducer.ts
@@ -1,8 +1,12 @@
 import { combineReducers } from "redux";
 import contactReducer from "./contacts/reducer";
+import apiLoadingReducer from "./api/loading";
+import apiErrorReducer from "./api/error";
 
 const rootReducer = combineReducers({
   contacts: contactReducer,
+  apiLoading: apiLoadingReducer,
+  apiError: apiErrorReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
