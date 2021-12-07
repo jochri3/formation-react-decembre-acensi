fbcaf7c199a117d5d87c62f2f97a409a9cafbb22
Christian Lisangola <jochri3@gmail.com>
2021-12-07 13:16:05 +0300

    feat:add action fetchContacts action creator

---
+++ src/store/contacts/action.creators.ts
@@ -0,0 +1,23 @@
+import { Dispatch } from "redux";
+import { fetchAll } from "../../services/contacts.api";
+import { ContactsAction } from "./actions";
+import ActionTypes from "./types";
+import { AxiosError } from "axios";
+
+export const fetchContacts =
+  () => async (dispatch: Dispatch<ContactsAction>) => {
+    dispatch({ type: ActionTypes.FETCH_CONTACTS_REQUEST });
+    fetchAll()
+      .then((response) =>
+        dispatch({
+          type: ActionTypes.FETCH_CONTACTS_SUCCESS,
+          payload: response.data,
+        })
+      )
+      .catch((error: AxiosError) => {
+        dispatch({
+          type: ActionTypes.FETCH_CONTACTS_FAILURE,
+          error: error?.response?.data?.message,
+        });
+      });
+  };
